4ea4d583feb050ccf99159e717c00e364fd4bb1b
Ian Skinner <Ian.Skinner01@googlemail.com>
2015-03-03 11:24:18 +0000

    png 1

--- plot1.R
+++ plot1.R
@@ -10,13 +10,13 @@ plotdata$Date <- as.Date(plotdata$Date, format = "%d/%m/%Y")
 plotdata <- subset(plotdata,Date >=as.Date("2007-02-01") & Date <=as.Date("2007-02-02"))
 plotdata$Time <- as.POSIXct(strptime(paste(plotdata$Date,plotdata$Time), format = "%Y-%m-%d %H:%M:%S"))
 
-# Open graphic device
+#Open Graphics Device
 windows()
 
 # Plot histogram
-hist(plotdata$Global_active_power, main = "Global Active Power", col= "Red", xlab = "Global Active Power (Kilowatts)") ) 
+hist(plotdata$Global_active_power, main = "Global Active Power", col= "Red", xlab = "Global Active Power (Kilowatts)") 
 
 #Copy to PNG format at 480x480
 
-dev.copy(png, filename = "plot1.png")
+dev.copy(png, filename = "plot1.png", width = 480, height = 480)
 dev.off()
