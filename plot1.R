ff360bbbda2913b0d33e3c7ef4bd05f363827ace
Ian Skinner <Ian.Skinner01@googlemail.com>
2015-03-03 11:16:48 +0000

    png 1

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## This code reads the power concumption data set 
+## and produced the viz for plot1 , outputting this to PNG
+
+
+#Load data, convert date times and subset to 2 day period
+plotdata <- read.table("household_power_consumption.txt", sep = ";", header=TRUE, na.strings = "?")
+
+plotdata$Date <- as.Date(plotdata$Date, format = "%d/%m/%Y")
+
+plotdata <- subset(plotdata,Date >=as.Date("2007-02-01") & Date <=as.Date("2007-02-02"))
+plotdata$Time <- as.POSIXct(strptime(paste(plotdata$Date,plotdata$Time), format = "%Y-%m-%d %H:%M:%S"))
+
+# Open graphic device
+windows()
+
+# Plot histogram
+hist(plotdata$Global_active_power, main = "Global Active Power", col= "Red", xlab = "Global Active Power (Kilowatts)") ) 
+
+#Copy to PNG format at 480x480
+
+dev.copy(png, filename = "plot1.png")
+dev.off()
